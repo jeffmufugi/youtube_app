6535e46ed1dc3c516f004611764ce4d9bbe787ba
MvsterJeffrey <mvsterjeffrey@Mufugis-MacBook-Air.local>
2021-02-22 13:28:55 +0800

    Initial xcode project creation

---
+++ youtube_app/youtube_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_appApp.swift
+//  youtube_app
+//
+//  Created by MvsterJeffrey on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct youtube_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
